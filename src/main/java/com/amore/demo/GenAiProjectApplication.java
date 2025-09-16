be2e477f5f7bdcbefb28038cd423798025b56bce
jiho lee <dlwlgh1254@naver.com>
2025-09-16 17:12:21 +0900

    Initial commit

---
+++ src/main/java/com/amore/demo/GenAiProjectApplication.java
@@ -0,0 +1,15 @@
+package com.amore.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenAiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenAiProjectApplication.class, args);
+	}
+
+    //
+
+}
